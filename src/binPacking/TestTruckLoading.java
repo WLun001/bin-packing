8f43f2f4f2207a92eccf97a12d8f6ca0fcbe4258
Wei Lun <weilun_95@hotmail.com>
2018-08-11 16:41:27 +0800

    testing algo

--- src/binPacking/TestTruckLoading.java
+++ src/binPacking/TestTruckLoading.java
@@ -12,28 +12,49 @@ public class TestTruckLoading {
     public static void main(String[] args) {
         int min = 1;
         int max = 10;
-        Object[] parcels = new Parcel[4];
-//        for (int i = 0; i < parcels.length; i++)
-//            parcels[i] = new bin.object.Parcel((int) (Math.random() * ((max - min) + 1)) + min);
-        parcels[0] = new Parcel(10);
-        parcels[1] = new Parcel(7);
-        parcels[2] = new Parcel(3);
-        parcels[3] = new Parcel(5);
+        Object[] parcels = new Parcel[100];
+        for (int i = 0; i < parcels.length; i++)
+            parcels[i] = new Parcel((int) (Math.random() * ((max - min) + 1)) + min);
+//        parcels[0] = new Parcel(10);
+//        parcels[1] = new Parcel(7);
+//        parcels[2] = new Parcel(3);
+//        parcels[3] = new Parcel(5);
+//        parcels[4] = new Parcel(10);
+//        parcels[5] = new Parcel(7);
+//        parcels[6] = new Parcel(3);
+//        parcels[7] = new Parcel(5);
+//        parcels[8] = new Parcel(7);
+//        parcels[9] = new Parcel(3);
+//        parcels[10] = new Parcel(3);
+//        parcels[11] = new Parcel(10);
+//        parcels[12] = new Parcel(7);
+//        parcels[13] = new Parcel(3);
+//        parcels[14] = new Parcel(5);
+//        parcels[15] = new Parcel(10);
+//        parcels[16] = new Parcel(7);
+//        parcels[17] = new Parcel(3);
+//        parcels[18] = new Parcel(5);
+//        parcels[19] = new Parcel(7);
 
+
+        long startTime = System.currentTimeMillis();
         BinPacking truckLoading = new TruckLoading();
-        Bin[] trucks = ((TruckLoading)truckLoading).bestFit(parcels);
+        Bin[] trucks = ((TruckLoading)truckLoading).firstFitDecreasing(parcels);
+        long stopTime = System.currentTimeMillis();
+        System.out.print(stopTime - startTime + "ms");
 
-        for (int i = 0; i < trucks.length; i++) {
-            Truck truck = (Truck) trucks[i];
-            if (truck.getParcels().size() > 0) {
-                System.out.println("bin.Truck ID: " + truck.getId());
-                System.out.println("Boxes: " + truck.getParcels().size());
-                truck.getParcels().forEach(j -> System.out.println("Weight per box: " + j.getWeight()));
-                System.out.println("bin.Truck current load: " + truck.getCurrentLoad());
-            }
-        }
-
-        System.out.println("Total trucks used: " + Arrays.stream(trucks).filter(i -> ((Truck) i).getParcels().size() > 0).count());
+//
+//        for (int i = 0; i < trucks.length; i++) {
+//            Truck truck = (Truck) trucks[i];
+//            if (truck.getParcels().size() > 0) {
+//                System.out.println("bin.Truck ID: " + truck.getId());
+//                System.out.println("Boxes: " + truck.getParcels().size());
+//                truck.getParcels().forEach(j -> System.out.println("Weight per box: " + j.getWeight()));
+//                System.out.println("bin.Truck current load: " + truck.getCurrentLoad());
+//            }
+//        }
+//
+//        System.out.println("Total trucks used: " + Arrays.stream(trucks).filter(i -> ((Truck) i).getParcels().size() > 0).count());
     }
 
 }
